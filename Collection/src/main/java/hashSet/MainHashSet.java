e6d434dc267636d35caadb06ad05a6a01a3e68f2
slawo <slawekpettke@gmail.com>
2022-09-13 22:19:08 +0200

    hashset

---
+++ Collection/src/main/java/hashSet/MainHashSet.java
@@ -0,0 +1,10 @@
+package hashSet;
+
+import java.util.HashSet;
+
+public class MainHashSet {
+    public static void main(String[] args) {
+
+        HashSet<String> set = new HashSet<>();
+    }
+}

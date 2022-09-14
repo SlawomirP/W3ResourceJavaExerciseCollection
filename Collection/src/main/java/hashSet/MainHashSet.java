84f7e478bcf3b8dea48a5a63947f5da4bd654aba
slawo <slawekpettke@gmail.com>
2022-09-14 22:02:22 +0200

    hashset

--- Collection/src/main/java/hashSet/MainHashSet.java
+++ Collection/src/main/java/hashSet/MainHashSet.java
@@ -6,5 +6,26 @@ public class MainHashSet {
     public static void main(String[] args) {
 
         HashSet<String> set = new HashSet<>();
+        set.add("one");
+        set.add("two");
+        set.add("three");
+        set.add("four");
+        set.add("five");
+
+        iterate(set);
+        System.out.println("--> " + size(set));
+
+    }
+
+    //Write a Java program to iterate through all elements in a hash list
+    public static void iterate(HashSet<String> set){
+        set.stream().forEach(System.out::println);
+    }
+
+    //Write a Java program to get the number of elements in a hash set.
+    public static long size(HashSet<String> set){
+        long sizeSet = set.stream()
+                .count();
+       return sizeSet;
     }
 }

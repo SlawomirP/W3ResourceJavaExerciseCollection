19e699f7cf994399427729caabbc7bcaa7b8fc1c
slawo <slawekpettke@gmail.com>
2022-09-15 21:53:50 +0200

    hashset

--- Collection/src/main/java/hashSet/MainHashSet.java
+++ Collection/src/main/java/hashSet/MainHashSet.java
@@ -12,11 +12,29 @@ public class MainHashSet {
         set.add("four");
         set.add("five");
 
-        iterate(set);
+        HashSet<String> set2 = new HashSet<>();
+        set2.add("six");
+        set2.add("seven");
+        set2.add("eight");
+        set2.add("nine");
+        set2.add("ten");
+
+
+        set.stream()
+                .forEach(value -> System.out.println(value));
         System.out.println("--> " + size(set));
         clearSet(set);
         iterate(set);
         System.out.println("---");
+        System.out.println(numberElements(set));
+        System.out.println(isEmpty(set));
+
+        System.out.println("+++++++++++");
+        set = (HashSet)set2.clone();
+        iterate(set);
+        System.out.println("+++++++++++");
+
+
 
     }
 
@@ -35,5 +53,15 @@ public class MainHashSet {
     //Write a Java program to empty an hash set.
     public static void clearSet(HashSet<String> set){
         set.removeAll(set);
+    }
+
+    //program to get the number of elements in a hash set
+    public static int numberElements(HashSet<String> set){
+        return set.size();
+    }
+
+    //program to test a hash set is empty or not.
+    public static boolean isEmpty(HashSet<String> set){
+        return set.isEmpty();
     }
 }
